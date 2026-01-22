a98b4239ae79d574f0fb7e068d14f23d09c5713f
Yiannis <yiannis.leblanc@icloud.com>
2026-01-22 10:41:01 +0100

    Complétion run et run_aux

--- src/program/run.c
+++ src/program/run.c
@@ -7,27 +7,45 @@
 // Returns true if a Return statement was reached, stop the execution
 // Returns false if the end of a block was reached
 bool run_aux(int var_value[], const t_ast *prog) {
-
-    if (prog == NULL)
-        return false;
-
-    switch (prog->command) {
+    bool returnValue = false;
+    t_ast* tempProg = prog;
+    while(tempProg!=NULL && !returnValue){
+    switch (tempProg->command) {
 
         case Return: {
             // prog->command == Return, so we assume that the statement has type t_return_statement
-            t_return_statement st = prog->statement.return_st;
-            // TODO
-            return false; // TODO
+            t_return_statement st = tempProg->statement.return_st;
+            printf("->%d\n",eval_rpn(var_value, &st.expr));
+            returnValue = true;
         }
-            // TODO
+            break;
+        case Assignment:
+            var_value[tempProg->statement.assignment_st.var-'a'] = eval_rpn(var_value, &tempProg->statement.assignment_st.expr);
+            break;
+        case If:
+            if(eval_rpn(var_value, &tempProg->statement.if_st.cond)){
+                returnValue = run_aux(var_value, tempProg->statement.if_st.if_true);
+            }else{
+                returnValue = run_aux(var_value, tempProg->statement.if_st.if_false);
+            }
+            break;
+        case While:
+            while(eval_rpn(var_value, &tempProg->statement.while_st.cond) && !returnValue){
+                returnValue = run_aux(var_value, tempProg->statement.while_st.block);
+            }
+            break;
+        case Print:
+            printf("%d\n",eval_rpn(var_value, &tempProg->statement.print_st.expr));
+            break;
         default:
             break;
+        }
+        tempProg = tempProg->next;
     }
-
-    return false; // TODO
+    return returnValue;
 }
 
 void run(t_ast *prog) {
-
-    // TODO
+    int var_value['z'-'a'+1];
+    run_aux(var_value, prog);
 }
