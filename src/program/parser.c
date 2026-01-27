cc2b795a19aef33214516e04e159aa2576446d3f
Yiannis <yiannis.leblanc@icloud.com>
2026-01-27 13:25:14 +0100

    Complétion fonctions parse() et parse_aux(): parse appelle parse_aux.

--- src/program/parser.c
+++ src/program/parser.c
@@ -20,7 +20,6 @@ void get_expr(t_expr_rpn *expr, const t_prog_token_list *list, unsigned int *i) 
 }
 
 t_ast *parse_aux(const t_prog_token_list *list, unsigned int *i) {
-
     if (*i >= list->size)
         return NULL;
 
@@ -29,36 +28,77 @@ t_ast *parse_aux(const t_prog_token_list *list, unsigned int *i) {
     const t_prog_token token = ptl_get(list, *i);
     switch (token.token_type) {
         case PT_VAR: {
-            // TODO
             t_assignment_statement st;
-            st.var = 'a'; // TODO
+            st.var = token.content.var;
+            (*i)++;
+            if(ptl_get(list, *i).token_type!=Assignment){
+                printf("Variable non suivie par une affectation !\n");
+                return NULL;
+            }
+            (*i)++;
             get_expr(&st.expr, list, i);
-            // TODO
-            // prog->... = ...
+            prog->command = Assignment;
+            prog->statement.assignment_st = st;
             break;
         }
         case PT_KEYWORD: {
             switch (token.content.keyword) {
                 case KW_PRINT: {
-                    // TODO
+                    t_print_statement st;
+                    (*i)++;
+                    if(ptl_get(list, *i).token_type != PT_EXPR){
+                        printf("Print n'est pas suivi d'une expression !\n");
+                        return NULL;
+                    }
+                    (*i)++;
+                    get_expr(&st.expr,list , i);
+                    prog->command = Print;
+                    prog->statement.print_st = st;
                     break;
                 }
                 case KW_RETURN: {
                     t_return_statement st;
-                    // TODO
+                    (*i)++;
+                    if(ptl_get(list, *i).token_type != PT_EXPR){
+                        printf("Return n'est pa suivi d'une expression !\n");
+                        return NULL;
+                    }
+                    (*i)++;
+                    get_expr(&st.expr,list , i);
+                    prog->command = Return;
+                    prog->statement.return_st = st;
                     break;
                 }
                 case KW_IF: {
                     t_if_statement st;
                     st.if_true  = NULL;
                     st.if_false = NULL;
-                    // TODO
+                    (*i)++;
+                    if(ptl_get(list, *i).token_type != PT_EXPR){
+                        printf("IF n'est pas suivi d'une expression !\n");
+                        return NULL;
+                    }
+                    (*i)++;
+                    get_expr(&st.cond,list , i);
+                    st.if_true = parse_aux(list, i);
+                    st.if_false = parse_aux(list, i);
+                    prog->statement.if_st = st;
+                    prog->command = If;
                     break;
                 }
                 case KW_WHILE: {
                     t_while_statement st;
                     st.block = NULL;
-                    // TODO
+                    (*i)++;
+                    if(ptl_get(list, *i).token_type != PT_EXPR){
+                        printf("While n'est pas suivi d'une expression !\n");
+                        return NULL;
+                    }
+                    (*i)++;
+                    get_expr(&st.cond, list, i);
+                    st.block = parse_aux(list, i);
+                    prog->command = While;
+                    prog->statement.while_st = st;
                     break;
                 }
                 case KW_ENDBLOCK:
@@ -83,11 +123,11 @@ t_ast *parse_aux(const t_prog_token_list *list, unsigned int *i) {
         //free(prog);
         return NULL;
     }
+    prog->next = parse_aux(list, i);
     return prog;
 }
 
 t_ast *parse(const t_prog_token_list *list) {
-
     if (list->size == 0) return NULL;
 
     unsigned int i = 0; // index in the list
