f7c3b977720adc79b0e914c8cef3cd191a3f6b8d
Yiannis <yiannis.leblanc@icloud.com>
2026-02-04 10:43:20 +0100

    Bug fix

--- src/file_io/file.c
+++ src/file_io/file.c
@@ -4,7 +4,7 @@
 
 char *read_file(const char *file_name) {
     
-    FILE *file = fopen(file_name, "r");
+    FILE *file = fopen(file_name, "rb");
     if (file == NULL) {
         printf("Error: file %s not found\n", file_name);
         return NULL;
@@ -12,7 +12,7 @@ char *read_file(const char *file_name) {
     fseek(file, 0, SEEK_END);
     const unsigned int file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
-    char *buffer = malloc(file_size * sizeof(char));
+    char *buffer = malloc((file_size + 1) * sizeof(char));
     fread(buffer, sizeof(char), file_size, file);
     fclose(file);
     buffer[file_size] = '\0';
